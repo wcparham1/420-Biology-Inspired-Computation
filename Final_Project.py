1dc4a606476ada00a2b6eaa45ad7136495ad6712
wparham1 <wparham1@vols.utk.edu>
2023-04-29 14:00:16 -0500

    working on rescaling values for player selection

--- Final_Project.py
+++ Final_Project.py
@@ -4,66 +4,26 @@ import pandas as pd
 from geneal.genetic_algorithms import ContinuousGenAlgSolver, BinaryGenAlgSolver
 from geneal.applications.fitness_functions.continuous import fitness_functions_continuous
 
+def rescale_value(old_max, old_min, new_max, new_min, old_value):
+    old_range=(old_max - old_min)
+    new_range=(new_max - new_min)
+    new_value = (((old_value - old_min) * new_range) / old_range) + new_min
+
+    return new_value
 
 def merge(list1, list2):
      
     merged_list = tuple(zip(list1, list2))
     return merged_list
 
-class TemplateChildClass(ContinuousGenAlgSolver, BinaryGenAlgSolver):
-    def __init__(self, *args, **kwargs):
-        BinaryGenAlgSolver.__init__(self, *args, **kwargs)
-        ContinuousGenAlgSolver.__init__(self, *args, **kwargs)
+def my_fitness_function():    
+    ret_val = lambda chromosome: (1.5*chromosome[0] + 1.0*chromosome[1] + 2.0*chromosome[2])
+    return ret_val
 
-    #a chromosome will be an individual player
-    def fitness_function(self, chromosome):
-        """
-        Implements the logic that calculates the fitness
-        measure of an individual.
 
-        :param chromosome: chromosome of genes representing an individual
-        :return: the fitness of the individual
-        """
-        pass
-
-    def initialize_population(self):
-        """
-        Initializes the population of the problem
-
-        :param pop_size: number of individuals in the population
-        :param n_genes: number of genes representing the problem. In case of the binary
-        solver, it represents the number of genes times the number of bits per gene
-        :return: a numpy array with a randomized initialized population
-        """
-        pass
-
-    def create_offspring(
-        self, first_parent, sec_parent, crossover_pt, offspring_number
-    ):
-        """
-        Creates an offspring from 2 parents. It uses the crossover point(s)
-        to determine how to perform the crossover
-
-        :param first_parent: first parent's chromosome
-        :param sec_parent: second parent's chromosome
-        :param crossover_pt: point(s) at which to perform the crossover
-        :param offspring_number: whether it's the first or second offspring from a pair of parents.
-        Important if there's different logic to be applied to each case.
-        :return: the resulting offspring.
-        """
-        pass
-
-    def mutate_population(self, population, n_mutations):
-        """
-        Mutates the population according to a given user defined rule.
-
-        :param population: the population at a given iteration
-        :param n_mutations: number of mutations to be performed. This number is 
-        calculated according to mutation_rate, but can be adjusted as needed inside this function
-        :return: the mutated population
-        """
-        pass
-
+def perform_min_max_normalization(minv, maxv, prec, vals):
+    unique_values = int(((maxv-minv) / prec) + 1)
+    return ((vals - minv) / (maxv + prec - minv) * unique_values).astype(int)
 
 def main():
     
@@ -72,15 +32,9 @@ def main():
     season_2022_rows = df.loc[df['season']==2022]
     team_list = season_2022_rows['team_id'].unique()
     
-    print(season_2022_rows.columns)
-    # season_2022_rows = season_2022_rows.iloc[:, :-20]
-    # season_2022_rows = season_2022_rows.iloc[:,2:]
-    
     useful_stats = season_2022_rows[['fg_per_g', 'stl_per_g', 'blk_per_g']].copy()
-    print('da stats: \n', useful_stats.head)
-    #The important stats for what we are doing will be fg_per_g, stl_per_g, blk_per_g
-    print('\n da old stuff: \n', season_2022_rows['fg_per_g'].head)
-    # team_TOR = season_2022_rows.loc[season_2022_rows['team_id']=='TOR']
+    
+    # The important stats for what we are doing will be fg_per_g, stl_per_g, blk_per_g
 
     #iterate through each team contained in team_list and append each lineup to the rosters_2022 list
     rosters_2022 = []
@@ -96,27 +50,73 @@ def main():
     #look at each position and find the min/max of each statistic.
     mins = []
     maxs = []
-    for title in season_2022_rows.columns:
-        mins.append(season_2022_rows[title].min())
-        maxs.append(season_2022_rows[title].max())    
+    for col_name in useful_stats.columns:
+        mins.append(useful_stats[col_name].min())
+        maxs.append(useful_stats[col_name].max())    
     
-    min_maxs = list(merge(mins, maxs))
+    min_maxs_old = list(merge(mins, maxs))
     
     #Now that we have it sorted by position we can use the genetic algorithm to start "growing" the best player
     #Look into the readme for instructions on how to further use this library.  It is solving *something*
     #But not solving the lineup issue right now
 
-    # print("These are the Population sizes by position: ")
-    # for i in range(0, len(players_by_position)):
-    #     print('role: ', pos_list[i], 'pop_size: ', len(players_by_position[i]))
-
     solver = ContinuousGenAlgSolver(
-        n_genes=55,
-        fitness_function=fitness_functions_continuous(3),
-        pop_size=106,
+        n_genes=3,
+        fitness_function=my_fitness_function(),
+        pop_size=100,
         plot_results=False,
+        variables_limits=min_maxs_old,
+        random_state=42,
+        verbose=False,
     )
+    solver.solve()
+        
+    prev_fg_per_g = []
+    prev_stl_per_g = []
+    prev_blk_per_g = []
+    for member in solver.population_:
+        prev_fg_per_g.append(member[0])
+        prev_stl_per_g.append(member[1])
+        prev_blk_per_g.append(member[2])
 
-
+    new_mins = []
+    new_mins.append(min(prev_fg_per_g))
+    new_mins.append(min(prev_stl_per_g))
+    new_mins.append(min(prev_blk_per_g))
+    
+    new_maxs = []
+    new_maxs.append(max(prev_fg_per_g))
+    new_maxs.append(max(prev_stl_per_g))
+    new_maxs.append(max(prev_blk_per_g))
+    
+    scaled_member = [0,0,0]
+    
+    scaled_member[0] = rescale_value(
+                            old_min=min_maxs_old[0][0],
+                            old_max=min_maxs_old[0][1],  
+                            new_min=new_mins[0], 
+                            new_max=new_maxs[0], 
+                            old_value=solver.best_individual_[0]
+                            )
+    
+    print('scaled_member[0]:', scaled_member[0])
+    print(min_maxs_old[0][0], min_maxs_old[0][1], new_mins[0], new_maxs[0], solver.best_individual_[0])
+    
 if __name__ == "__main__":
     main()
+    
+    # y_interp = scipy.interpolate.interp1d()
+    
+    # solver.solve()
+    #print('this is best individual: ', solver.best_individual_)
+    # best_candidate = np.asarray(solver.best_individual_)
+    # best_candidate[0] = np.reshape(best_candidate[0], (-1,1))
+    # print('this is best candidate [0]', best_candidate[0])
+    
+    
+    # scaled_candidate = [0,0,0]
+    # scaled_candidate[0] = scaler.transform(best_candidate[0])
+    # print(scaled_candidate[0])
+    # scaled_candidate[0] = perform_min_max_normalization(minv = min_maxs[0][0], maxv=min_maxs[0][1], prec=.001, vals=best_candidate[0])
+    
+    # print(scaled_candidate[0])
